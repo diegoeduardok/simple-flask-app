bf4dbc102d1d9c5e28b5678030a4af043aaa1e0d
diegoeduardok <diegokleiman@hotmail.com>
2024-09-29 17:25:21 -0500

    Updated flask_uploads usage, fixed image size

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 from flask_uploads import UploadSet
 
-photos = UploadSet('photos', extensions=('jpg'))
+photos = UploadSet('photos', extensions=('jpg', 'png'))
 
 class UploadForm(FlaskForm):
     photo = FileField("Picture" ,validators=[FileAllowed(photos, 'Image Only!'), FileRequired('Choose a file!')])
--- application/routes.py
+++ application/routes.py
@@ -1,31 +1,17 @@
-from glob import glob
-import os
 import time
 from application import app
 from flask import render_template, redirect, url_for, request, flash
 from flask_uploads import UploadSet, configure_uploads, patch_request_class
 from application.forms import UploadForm, photos
-from application.secrets import upload_path #set upload path as appropriate
+from application.secrets import upload_path, secret_key #set upload path as appropriate
+from application.utils import *
 from model.load import predict
+
+app.config['UPLOADS_DEFAULT_DEST'] = upload_path
+app.config['SECRET_KEY'] = secret_key
 
 configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
-
-def which_animal(prediction):
-	if prediction['cat'] > 0.8:
-		return "cat"
-	elif prediction['dog'] > 0.8:
-		return "dog"
-	else:
-		return "non-identified animal :/"
-
-def clear_uploads(path=upload_path):
-	if not os.path.exists(path):
-		os.makedirs(path)
-
-	previous = glob(os.path.join(path, '*'))
-	for f in previous:
-		os.remove(f)
 
 @app.route("/")
 @app.route("/index")
@@ -49,11 +35,14 @@ def application():
 
 	if form.validate_on_submit():
 		for filename in request.files.getlist('photo'):
+
 			name = str(time.time()).replace(".","")
 			photos.save(filename, name=name + ".")
+			extension = filename.filename.split(".")[-1]
+			assert (2 < len(extension) < 5)
 		upload = True
-		file_url = photos.url(name + ".jpg")
-		animal = which_animal(predict(photos.path(name + ".jpg")))
+		file_url = photos.url(name + "." + extension)
+		animal = which_animal(predict(photos.path(name + "." + extension)))
 		flash("Image uploaded successfully!", category="success")
 	else:
 		upload = False
